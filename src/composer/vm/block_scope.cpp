464a3c13e311b1bf2dcf6353e777dc9a4f2775cf
René Descartes Muala <renemuala@icloud.com>
2025-09-26 16:32:28 +0200

    feat: improved scope handling

--- src/composer/vm/block_scope.cpp
+++ src/composer/vm/block_scope.cpp
@@ -1,14 +1,14 @@
 //
 // Created by dte on 9/25/2025.
 //
-#include "block_scope.hpp"
 #include "for_scope.hpp"
+
 namespace zen::composer::vm
 {
     block_scope* block_scope::__unsafely_make(const enum type type)
-{
-    const auto it = type == in_for ? new for_scope : new block_scope;
-    it->type = type;
-    return it;
-}
+    {
+        const auto it = type == in_for ? new for_scope : new block_scope;
+        it->type = type;
+        return it;
+    }
 }
--- src/composer/vm/block_scope.hpp
+++ src/composer/vm/block_scope.hpp
@@ -4,36 +4,64 @@
 
 #pragma once
 #include "scope.hpp"
+#include "composer/composer.hpp"
 
 namespace zen::composer::vm
 {
     struct block_scope : public scope
     {
+        std::map<std::string, symbol> locals = {};
+        i64 stack_usage = {};
         enum return_status
         {
             no_return = 0,
             branched_return = 1,
             concise_return = 1 << 3,
         } return_status = {no_return};
+        std::optional<symbol> get_local(const std::string& name)
+        {
+            std::optional<symbol> target;
+            if (nested_scope)
+                target = nested_scope->get_local(name);
+            if (not target)
+            {
+                if (const auto it = locals.find(name); it != locals.end())
+                    target = it->second;
+            }
+            return target;
+        }
+        void set_local(const std::string& name, const std::shared_ptr<const zen::composer::type>& t)
+        {
+            if (nested_scope)
+            {
+                nested_scope->set_local(name, t);
+            } else
+            {
+                locals.emplace(name, symbol(name, t, stack_usage));
+                stack_usage += t->get_size();
+            }
+        }
 
         block_scope* nested_scope = {};
 
-        virtual void push(block_scope* scope)
+        virtual void __dncd__push(block_scope* scope)
         {
             if (nested_scope)
-                nested_scope->push(scope);
+                nested_scope->__dncd__push(scope);
             else
             {
                 nested_scope = scope;
             }
         }
 
-        virtual void pop()
+        /// do not call directly
+        i64 __dncd__pop()
         {
+            i64 nested_scope_usage = 0;
             if (nested_scope)
             {
                 if (nested_scope->nested_scope)
-                    return nested_scope->pop();
+                    return nested_scope->__dncd__pop();
                 if (nested_scope->type == in_if and nested_scope->return_status == concise_return)
                     return_status = branched_return;
                 else if (nested_scope->type == in_else_if and nested_scope->return_status == concise_return)
@@ -42,9 +70,11 @@ namespace zen::composer::vm
                     return_status = concise_return;
                 else
                     return_status = no_return;
+                nested_scope_usage = nested_scope->stack_usage;
                 delete nested_scope;
                 nested_scope = nullptr;
             }
+            return nested_scope_usage;
         }
 
         enum return_status get_return_status() const
@@ -60,11 +90,6 @@ namespace zen::composer::vm
                 return_status = status;
         }
 
-        [[nodiscard]] bool is(const enum type& t) const override
-        {
-            return t & this->type == t;
-        }
-
         template<typename T>
         T * current(const enum type type)
         {
@@ -75,6 +100,13 @@ namespace zen::composer::vm
         }
 
         static block_scope* __unsafely_make(const enum type type);
+
+        [[nodiscard]] bool is(const enum type &t) const
+        {
+            if (nested_scope) return nested_scope->is(t);
+            // fmt::println("bs {} & {} = {}", int(type), int(t), int(type & t));
+            return (type & t)  == t;
+        }
     };
 }
 
