4ccfea8e8d1df479b4b198a995148958099c6f24
René Descartes Muala <renemuala@icloud.com>
2025-12-16 20:22:38 +0200

    feat: added basic string library

--- src/builder/table.cpp
+++ src/builder/table.cpp
@@ -22,7 +22,34 @@ namespace zen::builder
         return result;
     }
 
-    std::expected<std::shared_ptr<value>, std::string> table::get_value(const std::string& name)
+    std::expected<std::shared_ptr<value>, std::string> table::get_field(std::shared_ptr<value> target,
+        const std::vector<std::string>& tokens, const std::function<std::shared_ptr<value>()>& pointer_creator)
+    {
+        if (target)
+        {
+            const auto new_target = pointer_creator();
+            new_target->type = target->type;
+            new_target->is_reference = target->is_reference;
+            target = new_target;
+            for (const auto & token : tokens)
+            {
+                std::optional<std::pair<types::stack::i64, std::shared_ptr<builder::type>>> result = target->type->
+                    get_field(token);
+                if (result.has_value())
+                {
+                    target->type = result->second;
+                    target->offset += result->first;
+                } else
+                {
+                    return std::unexpected(fmt::format("no such field {} in {}", token, target->type->name));
+                }
+            }
+            return target;
+        }
+        return std::unexpected(fmt::format("no such field {} in unit", tokens[0]));
+    }
+
+    std::expected<std::shared_ptr<value>, std::string> table::get_value(const std::string& name,  const std::function<std::shared_ptr<value>()>& pointer_creator)
     {
         if (name.empty())
         {
@@ -30,24 +57,20 @@ namespace zen::builder
         }
         std::vector<std::string> tokens = split_name(name);
         std::shared_ptr<value> target = function->get_scope(true)->get_local(tokens[0]);
-        if (target)
+        if (target and tokens.size() > 1)
         {
-            for (auto field_tuple = tokens.begin()+1; field_tuple != tokens.end(); ++field_tuple)
-            {
-                std::optional<std::pair<types::stack::i64, std::shared_ptr<builder::type>>> result = target->type->
-                    get_field(*field_tuple);
-                if (result.has_value())
-                {
-                    target->type = result->second;
-                    target->offset += result->first;
-                } else
-                {
-                    return std::unexpected(fmt::format("no such field {} in {}", *field_tuple, target->type->name));
-                }
-            }
-            return target;
+            tokens.erase(tokens.begin());
+            return get_field(target, tokens, pointer_creator);
         }
         return std::unexpected(fmt::format("no such symbol {}", tokens[0]));
+    }
+
+    std::expected<std::shared_ptr<value>, std::string> table::get_value(const std::string& name)
+    {
+        return get_value(name, [&]()
+        {
+            return this->function->set_local(zen::builder::function::_long(), "pointer::table");
+        });
     }
 
     std::shared_ptr<table> table::create(const std::shared_ptr<builder::function>& function, std::shared_ptr<builder::type> type)
---
+++ src/library/string.cpp
@@ -0,0 +1,75 @@
+//
+// Created by dte on 12/16/2025.
+//
+
+#include "string.hpp"
+
+#include "builder/table.hpp"
+
+namespace zen::library::string
+{
+    inline std::shared_ptr<builder::function> create_allocator(utils::constant_pool & pool)
+    {
+        const auto fn = zen::builder::function::create(pool, 0,false, "zen::allocate");
+        const auto type = builder::function::_string();
+        fn->set_return(type);
+        fn->gen<zen::allocate>(fn->ret, type->get_full_size(), fmt::format("{}::size:{}", type->name, type->get_full_size()));
+
+        auto pointer_creator = [&]()
+        {
+            return fn->set_local(builder::function::_long(), "pointer::string");
+        };
+
+        if (auto r = builder::table::get_field(fn->ret, {"string::len"}, pointer_creator); r.has_value())
+        {
+            fn->move(r.value(), fn->constant<i64>(0));
+        } else
+        {
+            throw exceptions::semantic_error(r.error(),0);
+        }
+        if (auto r = builder::table::get_field(fn->ret, {"string::data"}, pointer_creator); r.has_value())
+        {
+            fn->move(r.value(), fn->constant<i64>(0));
+        } else
+        {
+            throw exceptions::semantic_error(r.error(),0);
+        }
+        fn->return_implicitly();
+        fn->build();
+        return fn;
+    }
+
+    inline std::shared_ptr<builder::function> create_deallocator(utils::constant_pool & pool)
+    {
+        const auto fn = zen::builder::function::create(pool, 0,false, "zen::deallocate");
+        auto pointer_creator = [&]()
+        {
+            return fn->set_local(builder::function::_long(), "pointer::string");
+        };
+        const auto type = builder::function::_string();
+        const auto it = fn->set_parameter(type,"it");
+        const auto has_data = fn->set_local(builder::function::_bool(),"has_data");
+        std::shared_ptr<zen::builder::value> it_data;
+        if (auto r = builder::table::get_field(it, {"string::data"}, pointer_creator); r.has_value())
+        {
+            it_data = r.value();
+            fn->gen<zen::i64_to_boolean>(has_data, it_data);
+        } else
+            throw exceptions::semantic_error(r.error(), 0);
+        fn->branch(builder::scope::in_if, has_data, [&](auto & fb, auto&, auto&)
+        {
+            fn->gen<deallocate>(fn->dereference(it_data));
+        });
+        fn->gen<deallocate>(it);
+        fn->build();
+        return fn;
+    }
+
+    std::shared_ptr<zen::builder::library> create(utils::constant_pool & pool)
+    {
+        std::shared_ptr<zen::builder::library> library = zen::builder::library::create("string");
+        library->add(create_allocator(pool));
+        library->add(create_deallocator(pool));
+        return library;
+    }
+}
