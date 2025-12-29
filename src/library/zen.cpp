f89c7efd4fe371b8cb031120af0327f8b87033f0
René Descartes Muala <renemuala@icloud.com>
2025-12-29 11:09:05 +0200

    feat: get zen art function

--- src/library/zen.cpp
+++ src/library/zen.cpp
@@ -5,7 +5,7 @@
 #include "zen.hpp"
 
 #include "builder/table.hpp"
-
+#include "vm/vm.hpp"
 #ifndef KAIZEN_VERSION
 #define KAIZEN_VERSION "unspecified"
 #endif
@@ -21,10 +21,20 @@ namespace zen::library::zen
         return fn;
     }
 
+    inline std::shared_ptr<builder::function> create_get_zen_art(utils::constant_pool & pool)
+    {
+        const auto fn = ::zen::builder::function::create(pool, 0,false, "getZenArt");
+        fn->set_return(::zen::builder::function::_string());
+        fn->return_value(fn->constant<std::string>(std::string(ascii_art)));
+        fn->build();
+        return fn;
+    }
+
     std::shared_ptr<::zen::builder::library> create(utils::constant_pool & pool)
     {
         std::shared_ptr<::zen::builder::library> library = ::zen::builder::library::create("zen");
         library->add(create_get_zen_version(pool));
+        library->add(create_get_zen_art(pool));
         return library;
     }
 }
